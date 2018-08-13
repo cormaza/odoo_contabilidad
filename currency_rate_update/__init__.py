28edc2ede420a3e3d6ba5183b3ee6abea218aadc
Jhonny16 <tinny.1609@gmail.com>
2018-08-13 10:44:00 -0500

    Eliminando currency_rate

--- currency_rate_update/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from .services.currency_getter_interface import CurrencyGetterInterface
--- currency_rate_update/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import currency_rate_update
-from . import company
-from . import account_config_settings
